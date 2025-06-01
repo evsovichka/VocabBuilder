ccaaa5e70bccf48e225cd2fb88f7bf8a94c6bb2a
Yevsovych Kateryna <159715045+evsovichka@users.noreply.github.com>
2025-06-01 16:53:51 +0300

    add appbar for mobile

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,9 +1,25 @@
 import Logo from "../Logo/Logo";
+import css from "./AppBar.module.css";
 
-export default function AppBar() {
+export default function AppBar({ name = "User" }) {
+  const isLogged = true;
+
   return (
-    <>
+    <section className={css.section}>
       <Logo />
-    </>
+      {isLogged && (
+        <div className={css.userInfo}>
+          <p className={css.userName}>{name}</p>
+          <div>
+            <svg width="20" height="20">
+              <use href="/icons/icons.svg#icon-user" />
+            </svg>
+          </div>
+          <svg width="32" height="22" className={css.iconBurger}>
+            <use href="/icons/icons.svg#icon-burger" />
+          </svg>
+        </div>
+      )}
+    </section>
   );
 }
