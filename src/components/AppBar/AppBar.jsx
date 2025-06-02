55619856a95ecf378a78ea4580571a390509aa83
Yevsovych Kateryna <159715045+evsovichka@users.noreply.github.com>
2025-06-02 20:00:15 +0300

    update appBar for tablet

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,8 +1,12 @@
+import { useResizeWindow } from "../../utils/resizeWindow.js";
 import Logo from "../Logo/Logo";
 import css from "./AppBar.module.css";
 
 export default function AppBar({ name = "User" }) {
   const isLogged = true;
+
+  const sizeWindow = useResizeWindow();
+  const isMobile = sizeWindow < 768;
 
   return (
     <section className={css.section}>
@@ -11,11 +15,15 @@ export default function AppBar({ name = "User" }) {
         <div className={css.userInfo}>
           <p className={css.userName}>{name}</p>
           <div>
-            <svg width="20" height="20">
+            <svg width={isMobile ? 20 : 24} height={isMobile ? 20 : 24}>
               <use href="/icons/icons.svg#icon-user" />
             </svg>
           </div>
-          <svg width="32" height="22" className={css.iconBurger}>
+          <svg
+            width={isMobile ? 32 : 40}
+            height={isMobile ? 22 : 28}
+            className={css.iconBurger}
+          >
             <use href="/icons/icons.svg#icon-burger" />
           </svg>
         </div>
